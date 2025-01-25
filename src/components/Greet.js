cf9c8145762f85427af3e19cc1b2a75e18f28713
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-01-25 05:13:08 +0300

    FeatAdd Greet component using an arrow function an without

---
+++ src/components/Greet.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+
+//functional 
+
+
+// function Greet() {
+//     return <h1>Hello manuu</h1>
+// }
+
+
+//using Arrow function
+
+const Greet = () =>{
+    return <h1>hello manuu</h1>
+}
+
+export default Greet;
